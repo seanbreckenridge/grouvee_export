fd0a5cb4d4697fb3e79798191dfbd2661a1ef59b
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-23 02:31:55 -0700

    install scripts in bin dir

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     zip_safe=False,
     keywords="",
     entry_points={"console_scripts": ["grouvee_export = grouvee_export.__main__:main"]},
+    scripts=list(map(str, Path("bin").rglob("*"))),
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
